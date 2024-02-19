154fb4f1c86b058f6999e771786027878f44922f
Shifu <aemre3113@gmail.com>
2024-02-19 21:45:52 +0300

    Header banner alanı oluşturuldu.

---
+++ src/components/BannerSection.js
@@ -0,0 +1,17 @@
+"use client";
+import styled from "styled-components";
+
+const Banner = styled.div`
+  background: url(/images/banner-bg.png);
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  position: relative;
+  overflow: hidden;
+`;
+
+const BannerSection = () => {
+  return <></>;
+};
+
+export default BannerSection;
