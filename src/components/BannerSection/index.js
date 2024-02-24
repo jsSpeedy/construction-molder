cce3974493f7e5ed7d5da1a9e8db096d86d21e86
Emre Aydın <emreaydin6587@gmail.com>
2024-02-24 15:48:13 +0300

    BannerSection revize edildi.

---
+++ src/components/BannerSection/index.js
@@ -0,0 +1,29 @@
+"use client";
+import styled from "styled-components";
+import Header from "../Header";
+import Container from "./Container";
+import LeftInfo from "./LeftInfo";
+import RightInfo from "./RightInfo";
+
+const Banner = styled.div`
+  background: url(/images/banner-bg.png);
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  position: relative;
+  overflow: hidden;
+  background-attachment: fixed;
+`;
+
+const BannerSection = () => {
+  return (
+    <Banner>
+      <Header />
+      <Container />
+      <LeftInfo />
+      <RightInfo />
+    </Banner>
+  );
+};
+
+export default BannerSection;
